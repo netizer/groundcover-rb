40f85930cda66153b6c7b47c8af53b6570a43eb9
Krzysiek Herod <krzysiek.herod@gmail.com>
2020-06-16 19:17:53 +0200

    Fix groundcover capability

--- lib/groundcover.rb
+++ lib/groundcover.rb
@@ -2,10 +2,14 @@ require 'byebug'
 require 'interpreter'
 
 module Groundcover
-  def groundcover__forest_parse_to_forest(node)
-    file_name = node[:children][0][:command]
+  # TODO: not used at the moment
+  def groundcover__forest_parse_text_to_forest(node)
+    files_content = node.join("\n")
+    eval_text(files_content)
+  end
+
+  def groundcover__forest_parse_to_forest(file_name)
     interpreter = Interpreter.new(direction = :gc_to_forest)
-    forest_tree = interpreter.eval_file(file_name)
-    evaluate(forest_tree)
+    interpreter.eval_file(file_name)
   end
 end
